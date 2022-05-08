d74acad7b299425295f474c2239aae6ef7aca9fe
azamaufar <azamaufar81@gmail.com>
2022-05-08 11:42:10 +0700

    Menambahkan script sampling.sh

---
+++ sampling.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+rm data/*
+
+#download data
+wget -P ./data https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+
+#convert setiap sheet
+
+FILE=/home/azamaufar/latihan_shell/bikin_bash_script/data/weather_data.xlsx
+if [[ -f "$FILE" ]]; then
+	echo "file exist"
+	in2csv data/weather_data.xlsx --sheet "weather_2014" > data/weather_2014.csv
+	in2csv data/weather_data.xlsx --sheet "weather_2015" > data/weather_2015.csv
+else
+	echo "Data belum terdownload"
+fi
+
+#Gabungkan data weather 2015 dan 2014 menjadi 1 csv 
+csvstack data/weather_2014.csv data/weather_2015.csv | csvlook > data/weather.csv
+rm data/weather_data.xlsx
+
+#sampling pada weather.csv dengan rate 0.3
+sample -r 0.3 data/weather.csv > data/sample_weather.csv
+
